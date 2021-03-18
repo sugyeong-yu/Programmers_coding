0a5aa289491763dafed310c9aa8192e435fa9de3
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-03-18 12:33:59 +0900

    Update k번째 수.py

--- level1/k번째 수.py
+++ level1/k번째 수.py
@@ -7,3 +7,8 @@ def solution(array, commands):
         answer.append(cut[c[2]-1])
             
     return answer
+
+
+#다른사람 정답
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
