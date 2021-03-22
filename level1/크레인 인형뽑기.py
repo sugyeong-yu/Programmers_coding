b3b522ab91c51b1011aa4976660137e5a338d2b5
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-03-22 14:17:49 +0900

    Update 크레인 인형뽑기.py

--- level1/크레인 인형뽑기.py
+++ level1/크레인 인형뽑기.py
@@ -17,3 +17,23 @@ def solution(board, moves):
                 break 
         
     return answer
+
+## 다른사람 풀이
+def solution(board, moves):
+    stacklist = []
+    answer = 0
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                stacklist.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(stacklist) > 1:
+                    if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1)
+                        stacklist.pop(-1)
+                        answer += 2     
+                break
+
+    return answer
