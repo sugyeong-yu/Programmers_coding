7c69225f94aaa73c82ec55b0da6414892b4fb4c4
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-03-22 13:31:35 +0900

    Update 체육복.py

--- level1/체육복.py
+++ level1/체육복.py
@@ -1,20 +1,23 @@
 def solution(n, lost, reserve):
-    borrow=len(lost)
-    for j in range(borrow):
-        if lost[j] in reserve:
-            lost.remove(lost[j])
-            reserve.remove(lost[j])
+    borrow=[]
+    #print(borrow)
+    for j in range(1,n+1):
+        #print(lost[j])
+        if j in reserve and j in lost:
+            lost.remove(j)
+            reserve.remove(j)
             print("Find! -> ",reserve)
-    for i in range(borrow):
+    for i in range(len(lost)):
         print("Not Find! -> ",reserve)
         if lost[i]-1 in reserve :
             print(lost[i],"에게 ", lost[i]-1,"가 체육복을 빌려준다.")
-            lost.remove(lost[i])
+            borrow.append(lost[i])
+            print(borrow)
             reserve.remove(lost[i]-1)
         elif lost[i]+1 in reserve:
             print(lost[i],"에게 ", lost[i]+1,"가 체육복을 빌려준다.")
-            lost.remove(lost[i])
+            borrow.append(lost[i])
+            print(borrow)
             reserve.remove(lost[i]+1)
-    print(borrow)
-    answer=n-len(lost)
+    answer=n-len(lost)+len(borrow)
     return answer
