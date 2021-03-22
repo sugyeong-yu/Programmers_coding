97ba4a0e63f235706e2a97d0460bacc70535ecad
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-03-22 13:31:55 +0900

    Update 체육복.py

--- level1/체육복.py
+++ level1/체육복.py
@@ -21,3 +21,16 @@ def solution(n, lost, reserve):
             reserve.remove(lost[i]+1)
     answer=n-len(lost)+len(borrow)
     return answer
+
+##다른사람 풀이
+def solution(n, lost, reserve):
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+    for r in _reserve:
+        f = r - 1
+        b = r + 1
+        if f in _lost:
+            _lost.remove(f)
+        elif b in _lost:
+            _lost.remove(b)
+    return n - len(_lost)
