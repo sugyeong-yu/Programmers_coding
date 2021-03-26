7f46d8f8061f6286ecc22c472ec498a2a7d05b08
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-03-26 16:49:03 +0900

    Update 가운데글자뽑기.py

--- level1/가운데글자뽑기.py
+++ level1/가운데글자뽑기.py
@@ -7,3 +7,9 @@ def solution(s):
         answer=s[(len(s)//2)-1:(len(s)//2)+1]
         print(answer)
     return answer
+
+#다른사람
+def string_middle(str):
+    # 함수를 완성하세요
+
+    return str[(len(str)-1)//2:len(str)//2+1]
