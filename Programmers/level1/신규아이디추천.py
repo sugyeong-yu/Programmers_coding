0626009f65f2a1f7cd033a0cf929e16169735ae1
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-04-20 19:13:45 +0900

    Update 신규아이디추천.py

--- Programmers/level1/신규아이디추천.py
+++ Programmers/level1/신규아이디추천.py
@@ -58,3 +58,38 @@ def solution(new_id):
 
     answer=''.join(re_id)
     return answer
+
+#2차
+import re
+
+def solution(new_id):
+    answer = ''
+    # 1단계 & 2단계 : 소문자 치환
+    answer = re.sub('[^a-z\d\-\_\.]', '', new_id.lower())
+    # 3단계 : 마침표 2번 이상 > 하나로
+    answer = re.sub('\.\.+', '.', answer)
+    # 4단계 : 양 끝 마침표 제거
+    answer = re.sub('^\.|\.$', '', answer)
+    # 5단계 : 빈 문자열이면 a 대입
+    if answer == '':
+        answer = 'a'
+    # 6단계 : 길이가 16자 이상이면 1~15자만 남기기 & 맨 끝 마침표 제거
+    answer = re.sub('\.$', '', answer[0:15])
+    # 7단계 : 길이가 3이 될 때까지 반복해서 끝에 붙이기
+    while len(answer) < 3:
+        answer += answer[-1:]
+    return answer
+
+#다른사람풀이
+import re
+
+def solution(new_id):
+    st = new_id
+    st = st.lower()
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+    st = re.sub('\.+', '.', st)
+    st = re.sub('^[.]|[.]$', '', st)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+    return st
