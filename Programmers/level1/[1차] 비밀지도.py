8838948ca8a310b96e8346f201e171be0701a6ba
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-05-13 18:00:21 +0900

    Update [1차] 비밀지도.py

--- Programmers/level1/[1차] 비밀지도.py
+++ Programmers/level1/[1차] 비밀지도.py
@@ -32,7 +32,7 @@ def solution(n, arr1, arr2):
     answer = []
     for i,j in zip(arr1,arr2):
         a12 = str(bin(i|j)[2:])
-        a12=a12.rjust(n,'0')
+        a12=a12.rjust(n,'0') #문자열 추가 "2".rjust(3, "0") >> "002"
         a12=a12.replace('1','#')
         a12=a12.replace('0',' ')
         answer.append(a12)
