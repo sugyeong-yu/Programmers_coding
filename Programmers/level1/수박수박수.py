1df70e243383019067c6a250baa55b964d0ce0ce
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-05-27 19:16:57 +0900

    Create 수박수박수.py

---
+++ Programmers/level1/수박수박수.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = ''    
+    for i in range(n):
+        if i%2==0:
+            answer+='수'
+        else :
+            answer+='박'
+    return answer
+  
+  # 다른사람풀이
+  def water_melon(n):
+    s = "수박" * n
+    return s[:n]
