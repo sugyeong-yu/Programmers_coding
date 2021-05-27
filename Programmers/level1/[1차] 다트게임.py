7e040eba5bf280d2caaa6b871b322d6ef84ea390
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2021-05-27 19:09:16 +0900

    Update [1차] 다트게임.py

--- Programmers/level1/[1차] 다트게임.py
+++ Programmers/level1/[1차] 다트게임.py
@@ -30,3 +30,26 @@ def solution(dartResult):
             answer[-1]=answer[-1]*(-1)
     answer=sum(answer)
     return answer
+
+# 다른사람풀이
+def solution(dartResult):
+    answer = []
+    dartResult = dartResult.replace('10','k')
+    point = ['10' if i == 'k' else i for i in dartResult]
+    print(point)
+
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt :
+            answer[i] = answer[i] ** (sdt.index(j)+1)
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0 :
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else:
+            answer.append(int(j))
+            i += 1
+    return sum(answer)
