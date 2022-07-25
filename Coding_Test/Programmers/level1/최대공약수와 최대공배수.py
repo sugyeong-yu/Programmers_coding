667f5ed03b986b46fb8753929d8db93ef8f8f978
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2022-07-25 16:33:07 +0900

    Update 최대공약수와 최대공배수.py

--- Coding_Test/Programmers/level1/최대공약수와 최대공배수.py
+++ Coding_Test/Programmers/level1/최대공약수와 최대공배수.py
@@ -1,3 +1,5 @@
+
+
 def solution(n, m):
     answer = []
     # 최대공약수
@@ -12,3 +14,20 @@ def solution(n, m):
             answer.append(i)
             break
     return answer
+
+#다른사람 풀이
+# 최대공약수-유클리드 호제법 / 최소공배수-input 2개를 곱하고 최대 공약수로 나눔
+# 유클리드호제법: 2개의 자연수 a, b에 대해서 a를 b로 나눈 나머지를 r이라 하면(단, a>b), a와 b의 최대공약수는 b와 r의 최대공약수와 같다.
+function solution(n, m) {
+    return [ gcd(n,m), (n*m) / gcd(n,m)]
+}
+
+function gcd(a, b) { // 단, a가 b보다 커야함.
+        let R;
+        while ((a % b) > 0)  {
+             R = a % b; //나머지
+                a = b;
+                b = R;
+        }
+         return b;
+        }
