062d646580a9492ab917289d3c471326d7e8783d
su gyeong <70633080+sugyeong-yu@users.noreply.github.com>
2022-08-29 14:15:58 +0900

    Update 시저암호.py

--- Coding_Test/Programmers/level1/시저암호.py
+++ Coding_Test/Programmers/level1/시저암호.py
@@ -2,6 +2,24 @@
 # .replace(' ',''): 모든 공백 제가
 
 def solution(s, n):
+    answer=[]
+    for w in s: 
+        if w==' ':
+            answer.append(w)
+            continue
+        if w.isupper():
+            next_ascii=ord(w)+n
+            if next_ascii>90:
+                next_ascii-=26
+        else:
+            next_ascii=ord(w)+n
+            if next_ascii>122:
+                next_ascii-=26
+        answer.append(chr(next_ascii))
+    return ''.join(answer)
+
+#다른사람풀이
+def solution(s, n):
     s = list(s)
     
     for i in range(len(s)):
